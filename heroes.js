797d8c0f066227d07f4ed4d39f8a2291d5c677cb
charles <charlesgomes@us.ibm.com>
2021-01-22 19:01:52 -0800

    Fixing cors

--- heroes.js
+++ heroes.js
@@ -49,7 +49,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.get('/heroes',cors(corsOptions), (req, res) => {
+app.get('/heroes',(req, res) => {
   console.log('Returning heroes list');
   client.query(query, (err, dbres) => {
     if (err) {
